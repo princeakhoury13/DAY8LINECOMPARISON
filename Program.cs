c52fea26dda5b01a62980c1039cf30b2742f19b3
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-17 10:54:23 +0530

    added: initialized object for Linemodelling class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            Console.WriteLine("Enter x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+            LineModelling newline = new LineModelling(x1, y1, x2, y2);
+            newline.Compare();
         }
     }
 }
