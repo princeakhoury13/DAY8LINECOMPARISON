583c9e3feca7136c77928970926ea43a0339d41c
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-17 10:42:52 +0530

    added: greeting message for homepage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DAY8LINES
+{
+    public class Lines
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+        }
+    }
+}
